8c2ecfa07ad61b1573ed36b919079aaeb609bc24
Matias Manero <matiasmanero@alumno.unc.edu.ar>
2018-03-01 20:09:16 -0300

    Seteo de temperatura/ Desarrolo en curso

--- BirraControl-v3.0.ino
+++ BirraControl-v3.0.ino
@@ -1,16 +1,23 @@
+#include <OneWire.h>                
+#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
+
+#define cant 3
+
+ 
+OneWire ourWire(3);                //Se establece el pin 3  como bus OneWire
+DallasTemperature sensors(&ourWire); //Se declara una variable u objeto para nuestro sensor
+//----------Seteo las direcciones de los sensores--------------------//
+DeviceAddress address1 = {0x28, 0xFF, 0x6B, 0x10, 0x86, 0x16, 0x5, 0xDB};//dirección del sensor 1
+DeviceAddress address2 = {0x28, 0xFF, 0xD3, 0xA0, 0x94, 0x16, 0x5, 0x3E};//dirección del sensor 2
+DeviceAddress address3 = {0x28, 0xFF, 0x3C, 0xA3, 0x94, 0x16, 0x5, 0x3};//dirección del sensor 3
+DeviceAddress address4 = {0x28, 0xFF, 0xE, 0x12, 0x1, 0x17, 0x4, 0x9D};  /// sensor de repuesto
 
 
-#include <Wire.h>
+#define BUTTON_ADC_PIN A0  // A0 is the button ADC input
 
-#include <LiquidCrystal.h>   // include LCD library
+#define LCD_BACKLIGHT_PIN 10  // D10 controls LCD backlight
 
-/*--------------------------------------------------------------------------------------
- Defines
- --------------------------------------------------------------------------------------*/
-// Pins in use
-#define BUTTON_ADC_PIN           A0  // A0 is the button ADC input
-
-#define LCD_BACKLIGHT_PIN         10  // D10 controls LCD backlight
 
 #define RIGHT_10BIT_ADC           0  // right
 #define UP_10BIT_ADC            120  // up
@@ -18,20 +25,55 @@
 #define LEFT_10BIT_ADC          480  // left
 #define SELECT_10BIT_ADC        720  // right
 #define BUTTONHYSTERESIS         30  // hysteresis for valid button sensing window
-//return values for ReadButtons()
+
+
+//-------------Configuro boton derecho------------------------------//
 #define BUTTON_NONE               0  // 
 #define BUTTON_RIGHT              1  // 
 #define BUTTON_UP                 2  // 
 #define BUTTON_DOWN               3  // 
 #define BUTTON_LEFT               4  // 
 #define BUTTON_SELECT             5  // 
-//some example macros with friendly labels for LCD backlight/pin control, tested and can be swapped into the example code as you like
-#define LCD_BACKLIGHT_OFF()     digitalWrite( LCD_BACKLIGHT_PIN, LOW )
-#define LCD_BACKLIGHT_ON()      digitalWrite( LCD_BACKLIGHT_PIN, HIGH )
-#define LCD_BACKLIGHT(state)    { if( state ){digitalWrite( LCD_BACKLIGHT_PIN, HIGH );}else{digitalWrite( LCD_BACKLIGHT_PIN, LOW );} }
-/*--------------------------------------------------------------------------------------
- Variables
- --------------------------------------------------------------------------------------*/
+
+//------------Seteo la LCD-----------------------------------------//
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+int lcd_key     = 0;
+int adc_key_in  = 0;
+
+
+//--------------------------Temp max and min de los fermentadores-----------------------//
+//float f_max[cant],f_min[cant];
+//float m_max[cant],m_min[cant];
+//------------------------------------------------------------------------------------//
+
+
+int derecha=0;
+
+//----------Configuro los pines para los interruptores------------//
+int Interruptor1 = A3; 
+int Interruptor2 = A4;
+int Interruptor3= A5;
+//----------Configuro los pines para los RELES--------//
+int RelePin = A1;
+int RelePin2 = 2;
+int RelePin3 = 1;
+int ReleMotor = A2;
+//----------seteo las variables------------//
+int value1=0;
+int value2=0;
+int value3=0;
+
+
+//int value[cant];
+int temp[cant];
+
+int prueba1=0;
+int prueba2=0;
+int prueba3=0;
+int bandera=0;
+
+boolean apagar[3];
+
 byte buttonJustPressed  = false;         //this will be true after a ReadButtons() call if triggered
 byte buttonJustReleased = false;         //this will be true after a ReadButtons() call if triggered
 byte buttonWas          = BUTTON_NONE;   //used by ReadButtons() for detection of button events
@@ -42,239 +84,193 @@ int scroll = 0;                         //Used in Menu(). Stores numerical value
 int button = 0;                         //Sets scroll when a button is pressed. Acts on it when release
 int menu = 4;                           //Stores the value of the menu. Used in Menu()
 int FunctionChoice;                     //Stores the value of the selected function
+
+int ChoiceButton;
+
 float Answer;                           //where the answer is stored.
 
 float A;                                //holds the values being computed
 float B;
-/*--------------------------------------------------------------------------------------
- Init the LCD library with the LCD pins to be used
- --------------------------------------------------------------------------------------*/
-LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );   //Pins for the freetronics 16x2 LCD shield. LCD: ( RS, E, LCD-D4, LCD-D5, LCD-D6, LCD-D7 )
-
-//------------------------------------------------------------------------------------
 
 
-void setup()
-{
-  //button adc input
-  pinMode( BUTTON_ADC_PIN, INPUT );         //ensure A0 is an input
-  digitalWrite( BUTTON_ADC_PIN, LOW );      //ensure pullup is off on A0
+
+
+void setup() {
+int i;
+  
+delay(1000);
+Serial.begin(9600);
+sensors.begin();               //Se inicia el sensor
+
+pinMode(Interruptor1, INPUT);  
+pinMode(Interruptor2, INPUT);
+pinMode(Interruptor3, INPUT);
+pinMode(RelePin, OUTPUT);
+pinMode(RelePin2, OUTPUT);
+pinMode(RelePin3, OUTPUT);
+pinMode(ReleMotor, OUTPUT);
+
+pinMode( BUTTON_ADC_PIN, INPUT );         //ensure A0 is an input
+digitalWrite( BUTTON_ADC_PIN, LOW );      //ensure pullup is off on A0
   //lcd backlight control
-  digitalWrite( LCD_BACKLIGHT_PIN, HIGH );  //backlight control pin D3 is high (on)
-  pinMode( LCD_BACKLIGHT_PIN, OUTPUT );     //D3 is an output
-  //set up the LCD number of columns and rows: 
-  lcd.begin( 16, 2 );
-  //Print some initial text to the LCD.
-  lcd.setCursor( 0, 0 );   //top left
-  lcd.print( "  Calculator    "  ); 
+digitalWrite( LCD_BACKLIGHT_PIN, HIGH );  //backlight control pin D3 is high (on)
+pinMode( LCD_BACKLIGHT_PIN, OUTPUT );     //D3 is an output
 
-  lcd.setCursor( 0, 1 );   //bottom left
-  lcd.print( "               " );
-  delay(1000);
-  Serial.begin(9600);
-  Serial.print ("We are in setup");      //Testing purposes
+lcd.begin(16, 2);                 //LCD de 16 columnas y 2 filas
+lcd.clear();                      //Borrar la pantalla
+lcd.setCursor(4, 0);              //Llevar el cursor al inicio
+lcd.print(" TOSKA ");             // Escritura del primer renglon 
+lcd.setCursor(0,1);               // Lo demas escribe en el segundo renglon
+
+
+for(i=0;i<cant;i++)
+  temp[i]=20;
 
 }
+ 
+void loop() {
+
+lcd.setCursor(0,1);            // Lleva el cursor al inicio de la segunda linea
+button = ReadButtons();  // Lee las teclas 
+value1= digitalRead(Interruptor1);// paso a digital el interruptor
+value2= digitalRead(Interruptor2);
+value3= digitalRead(Interruptor3);
+sensors.requestTemperatures();   //envía el comando para obtener las temperaturas
+float temp1= sensors.getTempC(address1);//Se obtiene la temperatura en °C del sensor 1
+temp[0]= sensors.getTempC(address1);
+float temp2= sensors.getTempC(address2);//Se obtiene la temperatura en °C del sensor 2
+float temp3= sensors.getTempC(address3);//Se obtiene la temperatura en °C del sensor 3
+
+if(ReadButtons()==BUTTON_SELECT)
+  Menu();
+
+//--------------------------------FERMENTADOR 1 -----------------------------------------------------------------------------//
+fermentar(0,ReleMotor,RelePin, temp1,value1,20,18,4,3);
+                                       //    |  |
+                                      //Fer max min Mad Max Min
+//--------------------------------FERMENTADOR 2 ---------------------------------------------------------------------------//
+fermentar(1,ReleMotor,RelePin2, temp2,value2,20,18,4,3);
+//--------------------------------FERMENTADOR 3-------------------------------------------------------------------------//
+fermentar(2,ReleMotor,RelePin3, temp3,value3,20,18,4,3);
 
 
-void loop ()
-{
-  SelectingMenu = true;
-  //function choice loop
-  //----------------------------------------------------------
-  while (SelectingMenu ==true)      
+if(apagar[0] && apagar[1] && apagar[2]) digitalWrite(ReleMotor, LOW);
+//------------------------------------------------------------------------//
+ChoiceButton=Menu2();
+
+switch (ChoiceButton){               // Imprime un texto según el valor de la tecla detectada
+  case 0:
   {
-    lcd.setCursor (0,0);
-    lcd.print ("Function:              ");
-
-    lcd.setCursor (0,1);
-
-    FunctionChoice = Menu();        //Get value for FunctionChoice
-
-
-    //Now we do something with the menu value we just got
-    //case 0 and 5 provide looping
-
-    switch (FunctionChoice)
-    {
-    case 0: 
-      {
-        FunctionChoice = 4;
-        menu = 4;
+    ChoiceButton=1;
+    menu=1;
+  }
+  case 1:
+  {
+      lcd.print("F1- ");
+      lcd.print(temp1);      // display seconds elapsed since power-up
+      lcd.print("C ");
+      if(value1==1){
+        lcd.print(" Fer");
+      }else {
+        lcd.print(" Mad");        
       }
-    case 1: 
-      {
-        lcd.print ("Add        (A+B)              ");
-        break;
-      }
+      break;
+    }
     case 2:
-      { 
-        lcd.print ("Subtract   (A-B)         ");
-        break;
+    {
+      lcd.print("F2- ");
+      lcd.print(temp2);      // display seconds elapsed since power-up
+      lcd.print("C ");
+      if(value2==1){
+        lcd.print(" Fer");
+      }else {
+        lcd.print(" Mad");        
       }
-    case 3: 
-      {
-        lcd.print ("Multiply   (A*B)        ");
-        break;
+      break;
+     }
+    case 3:
+    {
+      lcd.print("F3- ");
+      lcd.print(temp3);      // display seconds elapsed since power-up
+      lcd.print("C ");
+      if(value3==1){
+        lcd.print(" Fer");
+      }else {
+        lcd.print(" Mad");        
       }
+      break;
+    }
     case 4:
-      {
-        lcd.print ("Divide     (A/B)            ");
-        break;
+    {
+      ChoiceButton=1;
+      menu=1;
+      break;  
+    }
+     
+  }
+
+ 
+delay(100);                     
+}
+
+void fermentar(int n_ferm ,int Motor, int Pin, float temp,int value,float f_max,float f_min,float m_max,float m_min){
+
+if(value==1){ // entro en modo fermentacion
+  if (temp>=f_max){//------------------------------> camibiar temperatura minima modo fermentador 2
+    digitalWrite(Motor, HIGH);
+    digitalWrite(Pin, HIGH);   //  prendo rele 2
+    apagar[n_ferm]=false;
+  }else if (temp<f_min){
+    digitalWrite(Pin, LOW);   //  apago rele 2
+     apagar[n_ferm]=true; 
+    }
+}if(value==0) { //entro en modo maduracion
+  if (temp>=m_max){//------------------------------> camibiar temperatura minima modo maduracion 2
+    digitalWrite(Motor, HIGH);
+    digitalWrite(Pin, HIGH);   // Enciende el pin 9, prendo rele 2
+    apagar[n_ferm]=false;
+  }else if (temp<m_min){
+    digitalWrite(Pin, LOW);    //apago el rele 2
+    apagar[n_ferm]=true;
+   }
+}
+
+}
+
+void pantalla(){
+  int i;
+  for(i=0;i<3;i++){  
+    if (derecha==1){
+       lcd.print("F");
+       lcd.print(i+1);
+       lcd.print("- ");
+       lcd.print(temp[i]);      // display seconds elapsed since power-up
+       lcd.print("C ");
+       if(value1==1){
+          lcd.print(" Fer");
+        }else {
+          lcd.print(" Mad");        
       }
-    case 5: 
-      {
-        FunctionChoice = 1;
-        menu = 1;
-      }      
-    }  //End switch
-  }  // End while
-
-
-  // So if we get here, this means that the select button must have been pushed.
-  //FunctionChoice now holds the value of the function selected by the user.
-  lcd.setCursor (0,0);
-  lcd.print ("   Selection       ");
-  lcd.setCursor (0,1);
-  lcd.print ("   Confirmed            ");
-  delay (1000);  
-
-  //                         Get A and B
-  //-----------------------------------------------------------------------------
-
-  //Similar setup, this time get A and B. No looping
-
-  SelectingMenu = true;
-  while (SelectingMenu == true)                      //Get value for A
-  {
-    A = Menu();
-    lcd.setCursor (0,0);
-    lcd.print ("   Menu            ");
-    lcd.setCursor (0,1);
-    lcd.print ("A = ");
-    lcd.print (A);  
-    lcd.print ("                       ");  
-
+    }
   }
-  lcd.setCursor (0,0);
-  lcd.print ("   A: Selection        ");
-  lcd.setCursor (0,1);
-  lcd.print ("   Confirmed            ");
-  delay (1000);  
-
-
-
-  SelectingMenu = true;
-  while (SelectingMenu == true)                      //Get value for B
-  {
-    B = Menu();
-    lcd.setCursor (0,0);
-    lcd.print ("   Menu                ");
-    lcd.setCursor (0,1);
-    lcd.print ("B = ");
-    lcd.print (B);  
-    lcd.print ("                       ");  
-
-  }
-  lcd.setCursor (0,0);
-  lcd.print ("   B: Selection           ");
-  lcd.setCursor (0,1);
-  lcd.print ("   Confirmed            ");
-  delay (1000);  
-
-
-  //                       Compute and Output
-  //------------------------------------------------------------------------
-  lcd.setCursor (0,0);
-
-
-  switch (FunctionChoice)
-  {
-  case 0: 
-    {
-      FunctionChoice = 4;
-    }
-  case 1: 
-    {
-      lcd.print ("Add        (A+B)              ");
-      Add();
-      break;
-    }
-  case 2:
-    {
-      lcd.print ("Subtract   (A-B)         ");
-      Subtract();
-      break;
-    }
-  case 3: 
-    {
-      lcd.print ("Multiply   (A*B)        ");
-      Multiply();
-      break;
-    }
-  case 4:
-    {
-      lcd.print ("Divide     (A/B)            ");
-      Divide();
-      break;
-    }
-  case 5: 
-    {
-      FunctionChoice = 1;
-    }      
-  }  //End switch
-
-  lcd.setCursor (0,1);
-  lcd.print ("Answer = ");
-  lcd.print (Answer);  
-  lcd.print ("                       ");  
-  delay(2500);
-  lcd.setCursor (0,0);
-  lcd.print ("                  ");
-  lcd.setCursor (0,1);
-  lcd.print ("                       ");  
-  lcd.setCursor (0,1);
-} //End void loop
-//                   END LOOP
-//-------------------------------------------------------------------- 
-
-
-
-
-
-
-//---------------------------------------------------------------------------------
-//                         Computational Functions
-//---------------------------------------------------------------------------------
-
-int Add()
-{
-  Answer = A+B;
-  return Answer;
-}
-int Subtract()
-{
-  Answer = A-B;
-  return Answer;
-}
-int Multiply()
-{
-  Answer = A*B;
-  return Answer;
-}
-int Divide()
-{
-  Answer = A/B;
-  return Answer;
 }
 
+void Menu(){
+  int fermentador;
+  lcd.clear();                      //Borrar la pantalla
+  lcd.setCursor(2, 0);              //Llevar el cursor al inicio
+  lcd.print("Cambiar Temp");             // Escritura del primer renglon 
+  
+  while(ReadButtons()!= BUTTON_UP){}
+  
+  fermentador=choice_ferm();
+  
+  lcd.setCursor(0,1);               // Lo demas escribe en el segundo renglon
 
-//--------------------------------------------------------------------------------------
-//Allows the user to scroll up and down through a menu.
-//User must provide looping and Display
-//--------------------------------------------------------------------------------------
+  
+}
 
-int Menu ()
+int Menu2 ()
 {
   SelectingMenu = true;
   button = ReadButtons();
@@ -315,12 +311,100 @@ int Menu ()
   return menu;
 }  //End Function
 
+/*
+void set_temp(){
 
+while(read_LCD_buttons()!= btnLEFT){ 
+   switch (FunctionChoice)
+    {
+      case 0:{
+       lcd.print("T MAX");
+       lcd.print("- ");
+       lcd.print(temp[i]);      // display seconds elapsed since power-up
+       lcd.print("C ");
+       if(value1==1){
+          lcd.print(" Fer");
+        }else {
+          lcd.print(" Mad");  
+      }
+  case 1: 
+    {
+      lcd.print ("Add        (A+B)              ");
+      Add();
+      break;
+    }
+  case 2:
+    {
+      lcd.print ("Subtract   (A-B)         ");
+      Subtract();
+      break;
+    }
+  case 3: 
+    {
+      lcd.print ("Multiply   (A*B)        ");
+      Multiply();
+      break;
+    }
+  case 4:
+    {
+      lcd.print ("Divide     (A/B)            ");
+      Divide();
+      break;
+    }
+  case 5: 
+    {
+      FunctionChoice = 1;
+    }      
+  }  //End switch
+ 
+ }  
 
+}
+*/
 
+int choice_ferm(){
+  int caso=0, caso_ant=0;
+    
+  lcd.clear();                      //Borrar la pantalla
+  lcd.setCursor(0, 0);              //Llevar el cursor al inicio
+  lcd.print("Fermentacion 1");
+  
+  while(button!= BUTTON_LEFT){
+  
+  button=Menu2();
+  delay(150);
+  if(button== BUTTON_UP)
+    caso++;
+    
+  if(button== BUTTON_DOWN)
+    caso--;
+    
+   delay(500);
+   //if(caso>cant) caso=1;
+   
+   Serial.print("caso");
+   Serial.println(caso);
+  
+  
+  if(caso!=caso_ant){
+     
+      lcd.clear();                      //Borrar la pantalla
+      lcd.setCursor(0, 0);              //Llevar el cursor al inicio
+      Serial.print("pase");
+      
+      if((caso % 2)==0){lcd.print("Fermentacion ");lcd.print(caso);}
+      else {lcd.print("Maduracion ");
+        if(caso_ant==0) lcd.print("1");
+        else  lcd.print(caso_ant);
+        }
+      caso_ant=caso;
+  }
+       
+  }
+  
+  return caso;
+}
 
-
-//-------------------------------------------------------------------------------------------------  
 
 
 byte ReadButtons()
