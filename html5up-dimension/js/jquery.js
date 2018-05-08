dea77ceafb082b4615f5a59f4a86378201728baf
Marco <marcojadaro@gmail.com>
2018-05-08 17:53:22 -0300

    Nueva Pestaña

--- html5up-dimension/js/jquery.js
+++ html5up-dimension/js/jquery.js
@@ -1,28 +1,5 @@
 $(document).ready(function(){
-
-  $.ajax({
-    type: 'POST',
-    url: 'php/listas.php'
-  })
-  .done(function(listas_rep){
-    $('#listas').html(listas_rep)
-  })
-  .fail(function(){
-    alert('Hubo un errror al cargar las listas_rep')
-  })
-})
-$('#listas').on('change', function(){
-
-    var id = $('#lista').val()
-    $.ajax({
-      type: 'POST',
-      url: 'php/cargar_birra.php',
-      data: {'id': id}
-    })
-    .done(function(listas_rep){
-      $('#Cocinadas').val(listas_rep.1)
-    })
-    .fail(function(){
-      alert('Hubo un errror al cargar los vídeos')
-    })
-  })
+ $("btn1").click(function() {
+      $("#hola").html("Este es Hola Mundo con jQuery");
+   }); 
+});
